c43dd37c7e1cc37ba7a62434d67267604820c557
Eseoghene <emmanueltemiede@gmail.comm>
2023-08-10 01:40:01 +0100

    try again

--- manage.py
+++ manage.py
@@ -1,17 +1,17 @@
 from app import createapp, db
 from flask_migrate import upgrade, migrate, init, stamp
 from models import User
-
+app = createapp()
+   
+   
 def deploy():
     """Run Deployment tasks here"""
-    
-    app = createapp()
     with app.app_context():
-         init()
+        init()
         #  stamp()
         #  migrate()
-         upgrade()
-         db.create_all()
+        upgrade()
+        db.create_all()
 
     # Migrate database to the latest version
    
