b519db948e3fd94a3e7116859277381080f756a7
Eseoghene <emmanueltemiede@gmail.comm>
2023-08-21 13:43:30 +0100

    1

--- models.py
+++ models.py
@@ -6,10 +6,10 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
     
     id = db.Column(db.Integer, primary_key = True)
-    firstname = db.Column(db.String(80), unique = True, nullable = False)
-    lastname = db.Column(db.String(80), unique = True, nullable = False)
+    firstname = db.Column(db.String(80), unique = False, nullable = False)
+    lastname = db.Column(db.String(80), unique = False, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(300), unique = True, nullable = False)
+    password = db.Column(db.String(300), unique = False, nullable = False)
     
     def __repr__(self):
         return '<User %r>' % self.email
--- routes.py
+++ routes.py
@@ -121,52 +121,33 @@ def login():
 
 
 
-# Register route
-# @app.route("/register/", methods=("GET", "POST"), strict_slashes=False)
+# @app.route("/register", methods=("POST",), strict_slashes=False)
 # def register():
 #     """Register a new user."""
-#     form = register_form()
-#     if form.validate_on_submit():
-#         try:
-#             email = form.email.data
-#             pwd = form.pwd.data
-#             username = form.username.data
-            
-#             newuser = User(
-#                 username=username,
-#                 email=email,
-#                 pwd=bcrypt.generate_password_hash(pwd),
+#     try:
+#         data = request.get_json()
+#         email = data.get('email')
+#         password = data.get('password')
+#         firstname = data.get('firstname')
+#         lastname = data.get("lastname")
+        
+#         newuser = User(
+#             firstname=firstname,
+#             lastname = lastname,
+#             email=email,
+#             password=bcrypt.generate_password_hash(password),
 #             )
-    
-#             db.session.add(newuser)
-#             db.session.commit()
-#             flash(f"Account Succesfully created", "success")
-#             return redirect(url_for("login"))
 
-#         except InvalidRequestError:
-#             db.session.rollback()
-#             flash(f"Something went wrong!", "danger")
-#         except IntegrityError:
-#             db.session.rollback()
-#             flash(f"User already exists!.", "warning")
-#         except DataError:
-#             db.session.rollback()
-#             flash(f"Invalid Entry", "warning")
-#         except InterfaceError:
-#             db.session.rollback()
-#             flash(f"Error connecting to the database", "danger")
-#         except DatabaseError:
-#             db.session.rollback()
-#             flash(f"Error connecting to the database", "danger")
-#         except BuildError:
-#             db.session.rollback()
-#             flash(f"An error occured !", "danger")
-#     return render_template("auth.html",
-#         form=form,
-#         text="Create account",
-#         title="Register",
-#         btn_action="Register account"
-        # )
+#         db.session.add(newuser)
+#         db.session.commit()
+#         return {'message': 'Account Successfully created'}, 201
+
+#     except IntegrityError:
+#         db.session.rollback()
+#         return {'message': 'User already exists!'}, 409
+#     except Exception as e:
+#         db.session.rollback()
+#         return {'message': str(e)}, 500
 @app.route("/register", methods=("POST",), strict_slashes=False)
 def register():
     """Register a new user."""
@@ -176,24 +157,28 @@ def register():
         password = data.get('password')
         firstname = data.get('firstname')
         lastname = data.get("lastname")
-        
+
+        # Check if the email already exists in the database
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return {'message': 'User already exists!'}, 409
+
+        # If email doesn't exist, create a new user
         newuser = User(
             firstname=firstname,
-            lastname = lastname,
+            lastname=lastname,
             email=email,
             password=bcrypt.generate_password_hash(password),
-            )
+        )
 
         db.session.add(newuser)
         db.session.commit()
         return {'message': 'Account Successfully created'}, 201
 
-    except IntegrityError:
-        db.session.rollback()
-        return {'message': 'User already exists!'}, 409
     except Exception as e:
         db.session.rollback()
         return {'message': str(e)}, 500
+
 # Logout route to log out the user
 @app.route("/logout")
 @login_required
